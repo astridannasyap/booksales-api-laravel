a500c6d2563a783606bfc5b289f68c2c5da45d57
AstridAnnasyaPutri <astridputri564@gmail.com>
2026-04-22 18:57:05 +0700

    Tugas 3 API Postman

--- app/Http/Controllers/AuthorController.php
+++ app/Http/Controllers/AuthorController.php
@@ -10,6 +10,11 @@ class AuthorController extends Controller
     public function index(){
         $authors = Author::all();
 
-        return view('authors', ['authors' => $authors]);
+        return response()->json([
+            "success" => true,
+            "message" => "Get All Resource",
+            "data" => $authors
+
+        ], 200);
     }
 }
